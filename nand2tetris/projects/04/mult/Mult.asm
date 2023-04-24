00f372d59f5f79cb94afa5ab8df1d4ffb2f1b207
j-p-obrien <jpobrien925@gmail.com>
2023-04-24 11:54:27 -0700

    First try at project 4 mult implementation.

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,38 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// Note that R0*R1 = R0*(R1 - 1) + R0
+// We compute sum = sum + R0; R1 - 1 recursively until R1 = 0
+// Then we loop forever
+
+// R2 holds our output. Set equal to 0 at initialization
+@R2
+M = 0
+
+// Get number of iterations (R1) and put it in variable i
+@R1
+D = M
+@i
+M = D
+
+// If i != 0 then continue to the loop, otherwise we end.
+(END)
+@END
+D ; JEQ
+
+// Set D = R0
+@R0
+D = M
+
+// Compute R2 = R2 + D, i.e. R2 = R2 + R0
+@R2
+M = M + D
+
+// Decrement i by 1, and additionally store in D
+@i
+DM = M - 1
+
+// Unconditional jump to END, which will check to see if we continue the loop
+@END
+0; JMP
