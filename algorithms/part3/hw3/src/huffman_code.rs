fcf0f70b67555366d93a9afd1a59ff0aaaf09469
JP O'Brien <jpobrien925@gmail.com>
2023-11-09 15:38:49 -0800

    First commit for algorithms part 3 hw 3

---
+++ algorithms/part3/hw3/src/huffman_code.rs
@@ -0,0 +1,105 @@
+use std::collections::BinaryHeap;
+
+// Tree containing CodeNodes. Since we only care about the length of the codewords, we only need
+// to store the internal nodes of the encoding tree.
+#[derive(Debug, Clone)]
+pub struct HuffmanEncoding {
+    codes: Vec<CodeNode>,
+}
+
+// Internal node of the Huffman Encoding tree. We leaf out leaf nodes, which are simply the
+// original codewords.
+// left: index of left child in HuffmanEncoding vector
+// right: index of right child in HuffmanEncoding vector
+// If left/right is None, then that child is an original codeword. If it is Some(index),
+// then index is the index in HuffmanEncoding.codes.
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct CodeNode {
+    left: Option<usize>,
+    right: Option<usize>,
+}
+
+// Keeps track of the weights of the original and combined codewords. id is None if it is an
+// original codeword. id is Some(id) if it corresponds to an internal node on the Huffman tree.  If
+// this is the case, id is the index in HuffmanEncoding.codes
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+struct HeapCode {
+    id: Option<usize>,
+    weight: usize,
+}
+
+impl PartialOrd for HeapCode {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.weight.partial_cmp(&self.weight)
+    }
+}
+
+impl Ord for HeapCode {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+impl HuffmanEncoding {
+    pub fn encode(weights: Vec<usize>) -> Self {
+        let mut heap: BinaryHeap<_> = weights
+            .iter()
+            .map(|&w| HeapCode {
+                id: None,
+                weight: w,
+            })
+            .collect();
+
+        let mut codes = Vec::with_capacity(heap.len() - 1);
+        let mut id = 0;
+        while heap.len() > 1 {
+            let code1 = heap.pop().unwrap();
+            let code2 = heap.pop().unwrap();
+            codes.push({
+                CodeNode {
+                    left: code1.id,
+                    right: code2.id,
+                }
+            });
+            heap.push(HeapCode {
+                id: Some(id),
+                weight: code1.weight + code2.weight,
+            });
+            id += 1;
+        }
+
+        HuffmanEncoding { codes }
+    }
+
+    // Returns the minimum and maximum length of the codewords, respectively
+    pub fn min_max_length(&self) -> (usize, usize) {
+        let mut min = usize::MAX;
+        let mut max = usize::MIN;
+        let mut todo = vec![(self.codes[self.codes.len() - 1], 1)];
+        while let Some((current, depth)) = todo.pop() {
+            match (current.left, current.right) {
+                // Both children are leaf nodes
+                (None, None) => {
+                    max = max.max(depth);
+                    min = min.min(depth);
+                }
+                // left child is a leaf node, right is internal
+                (None, Some(right)) => {
+                    min = min.min(depth);
+                    todo.push((self.codes[right], depth + 1));
+                }
+                // right child is a leaf, left is internal
+                (Some(left), None) => {
+                    min = min.min(depth);
+                    todo.push((self.codes[left], depth + 1));
+                }
+                // both children are internal nodes
+                (Some(left), Some(right)) => {
+                    todo.push((self.codes[left], depth + 1));
+                    todo.push((self.codes[right], depth + 1));
+                }
+            }
+        }
+        (min, max)
+    }
+}
--- algorithms/part3/hw3/src/main.rs
+++ algorithms/part3/hw3/src/main.rs
@@ -1,3 +1,21 @@
+use crate::huffman_code::HuffmanEncoding;
+
+mod huffman_code;
+
 fn main() {
-    println!("Hello, world!");
+    let weights: Vec<usize> = std::fs::read_to_string("huffman.txt")
+        .unwrap()
+        .lines()
+        .skip(1)
+        .map(|w| w.parse())
+        .collect::<Result<_, _>>()
+        .unwrap();
+
+    let codes = HuffmanEncoding::encode(weights);
+
+    let (min_len, max_len) = codes.min_max_length();
+
+    println!("Maximum codeword length is: {}", max_len);
+
+    println!("Minimum codeword length is: {}", min_len)
 }
