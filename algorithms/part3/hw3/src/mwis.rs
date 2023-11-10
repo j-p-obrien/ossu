ea45f7bba90518ee7464de266f15af1e540c51b8
JP O'Brien <jpobrien925@gmail.com>
2023-11-09 18:36:47 -0800

    added tests for mwis

--- algorithms/part3/hw3/src/mwis.rs
+++ algorithms/part3/hw3/src/mwis.rs
@@ -14,14 +14,20 @@ impl MWIS {
         max_array[1] = self.weights[1];
         for vertex in 2usize..n {
             max_array[vertex] =
-                max_array[vertex - 1].max(max_array[vertex] + max_array[vertex - 2]);
+                max_array[vertex - 1].max(self.weights[vertex] + max_array[vertex - 2]);
         }
 
         // reconstruct
-        for vertex in (1..n).rev() {
-            included[vertex] = max_array[vertex] > max_array[vertex - 1];
+        let mut vertex: isize = n as isize - 1;
+        while vertex > 0 {
+            if max_array[vertex as usize] > max_array[vertex as usize - 1] {
+                included[vertex as usize] = true;
+                vertex -= 2;
+            } else {
+                vertex -= 1;
+            }
         }
-        included[0] = !included[1];
+        included[0] = vertex == 0;
 
         included
     }
@@ -29,6 +35,23 @@ impl MWIS {
 
 #[cfg(test)]
 mod tests {
+    use super::MWIS;
+
     #[test]
-    fn test_mwis() {}
+    fn test_mwis1() {
+        let vertices = MWIS {
+            weights: vec![1, 2, 3, 4],
+        };
+
+        assert_eq!(vertices.mwis(), vec![false, true, false, true]);
+    }
+
+    #[test]
+    fn test_mwis2() {
+        let vertices = MWIS {
+            weights: vec![1, 5, 2, 3, 4],
+        };
+
+        assert_eq!(vertices.mwis(), vec![false, true, false, false, true]);
+    }
 }
