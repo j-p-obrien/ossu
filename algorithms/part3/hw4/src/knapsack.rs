3332b680925461127cda1f052d24529db1a54b71
JP O'Brien <jpobrien925@gmail.com>
2023-11-17 11:03:04 -0800

    cleaned up algorithms part 3 hw4 impl

--- algorithms/part3/hw4/src/knapsack.rs
+++ algorithms/part3/hw4/src/knapsack.rs
@@ -1,5 +1,3 @@
-use std::{collections::BinaryHeap, ops::Add};
-
 type Value = usize;
 type Weight = usize;
 
@@ -14,40 +12,6 @@ pub struct Knapsack {
     pub items: Vec<Item>,
 }
 
-// reverse ord because heap is max heap by default, and we want lowest weight items to surface up
-// first.
-impl PartialOrd for Item {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        other.weight.partial_cmp(&self.weight)
-    }
-}
-
-impl Ord for Item {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other).unwrap()
-    }
-}
-
-impl Default for Item {
-    fn default() -> Self {
-        Self {
-            value: 0,
-            weight: 0,
-        }
-    }
-}
-
-impl Add for Item {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Item {
-            weight: self.weight + rhs.weight,
-            value: self.value + rhs.value,
-        }
-    }
-}
-
 impl Item {
     pub fn from_str(data: &str) -> Self {
         let item = data
@@ -60,15 +24,10 @@ impl Item {
             weight: item[1],
         }
     }
-
-    #[allow(dead_code)]
-    pub fn from(value: usize, weight: usize) -> Self {
-        Self { value, weight }
-    }
 }
 
 impl Knapsack {
-    pub fn from(data: &str) -> Self {
+    pub fn from_str(data: &str) -> Self {
         let mut lines = data.lines();
         let size = lines
             .next()
@@ -81,13 +40,7 @@ impl Knapsack {
         Self { size, items }
     }
 
-    pub fn max_value(&self) -> usize {
-        let results = self.create_knapsack_array().pop().unwrap();
-        let highest_value = results.into_iter().max_by_key(|item| item.value).unwrap();
-        highest_value.value
-    }
-
-    pub fn max_value2(&self) -> Value {
+    pub fn max_value(&self) -> Value {
         let max_weight = self.size;
         // at each iteration through the items of the knapsack, current_array[i] is the highest
         // value knapsack with weight up to and including i.
@@ -105,36 +58,10 @@ impl Knapsack {
         }
         *last_array.last().unwrap()
     }
-
-    // creates the array of values that contain the total budget used and the values of the
-    // knapsack with various items in it.
-    pub fn create_knapsack_array(&self) -> Vec<BinaryHeap<Item>> {
-        let n = self.items.len();
-        let mut array = vec![BinaryHeap::new(); n];
-        array[0].push(Item::default());
-        array[0].push(self.items[0]);
-        for i in 1..n {
-            dbg!(i);
-            array[i] = array[i - 1].clone();
-            let mut todo = array[i - 1].clone();
-            let current = self.items[i];
-            while let Some(item) = todo.pop() {
-                if current.weight + item.weight <= self.size {
-                    array[i].push(current + item)
-                } else {
-                    break;
-                }
-            }
-            dbg!(array[i].len());
-        }
-        array
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::BinaryHeap;
-
     use super::{Item, Knapsack};
 
     fn create_knapsack() -> Knapsack {
@@ -158,38 +85,8 @@ mod tests {
     }
 
     #[test]
-    fn test_array() {
-        let sack = create_knapsack();
-        let sack_array = vec![
-            vec![Item::from(10, 5), Item::default()],
-            vec![Item::from(3, 6), Item::from(10, 5), Item::default()],
-            vec![
-                Item::from(10, 10),
-                Item::from(17, 9),
-                Item::from(3, 6),
-                Item::from(10, 5),
-                Item::from(7, 4),
-                Item::default(),
-            ],
-        ];
-        let my_impl = sack
-            .create_knapsack_array()
-            .into_iter()
-            .map(BinaryHeap::into_sorted_vec)
-            .collect::<Vec<Vec<_>>>();
-
-        assert_eq!(my_impl, sack_array)
-    }
-
-    #[test]
     fn test_max_value() {
         let sack = create_knapsack();
         assert_eq!(sack.max_value(), 17)
-    }
-
-    #[test]
-    fn test_max_value2() {
-        let sack = create_knapsack();
-        assert_eq!(sack.max_value2(), 17)
     }
 }
--- algorithms/part3/hw4/src/main.rs
+++ algorithms/part3/hw4/src/main.rs
@@ -6,16 +6,16 @@ use knapsack::Knapsack;
 
 fn main() {
     let knapsack_data = fs::read_to_string("knapsack1.txt").unwrap();
-    let knapsack = Knapsack::from(&knapsack_data);
+    let knapsack = Knapsack::from_str(&knapsack_data);
     println!(
         "Max value of the first knapsack is: {}",
-        knapsack.max_value2()
+        knapsack.max_value()
     );
 
     let big_knapsack_data = fs::read_to_string(&"knapsack_big.txt").unwrap();
-    let big_knapsack = Knapsack::from(&big_knapsack_data);
+    let big_knapsack = Knapsack::from_str(&big_knapsack_data);
     println!(
         "Max value of the big knapsack is: {}",
-        big_knapsack.max_value2()
+        big_knapsack.max_value()
     );
 }
