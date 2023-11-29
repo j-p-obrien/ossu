f052b5d23844a33f1b44e604d5905bbc5eb4214a
JP O'Brien <jpobrien925@gmail.com>
2023-11-28 17:16:14 -0800

    Added lots of functionality for alogrithms part 4 hw1

---
+++ algorithms/part4/hw1/src/apsp.rs
@@ -0,0 +1,263 @@
+type Vertex = usize;
+type Dist = isize;
+
+// Edge for adjacency list
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub struct Edge {
+    head: Vertex,
+    distance: Dist,
+}
+
+// Adjacency list
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Graph(Vec<Vec<Edge>>);
+
+// Integers don't have inifinity but it sure would be cooler if they did
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub enum Distance {
+    Infinite,
+    Finite(Dist),
+}
+
+// Contains data on the shortest paths between two vertices
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct PathDistances(Vec<Vec<Distance>>);
+
+impl Distance {
+    pub fn min(&self, other: &Distance) -> Distance {
+        match (self, other) {
+            (Distance::Infinite, _) => *other,
+            (Distance::Finite(_), Distance::Infinite) => *self,
+            (Distance::Finite(w1), Distance::Finite(w2)) => {
+                if w1 <= w2 {
+                    *self
+                } else {
+                    *other
+                }
+            }
+        }
+    }
+}
+
+impl Edge {
+    pub fn from_str(data: &str) -> Self {
+        let (head, distance) = data.split_once(" ").unwrap();
+        Self {
+            head: head.parse().unwrap(),
+            distance: distance.parse().unwrap(),
+        }
+    }
+}
+
+impl Graph {
+    pub fn from_str(data: &str) -> Self {
+        let mut graph_data = data.lines();
+        // who needs error handling lol
+        let n_vertices: usize = graph_data
+            .next()
+            .unwrap()
+            .split(" ")
+            .next()
+            .unwrap()
+            .parse()
+            .unwrap();
+        let mut adjacency_list = vec![vec![]; n_vertices + 1];
+
+        for line in graph_data {
+            let (tail_data, edge_data) = line.split_once(" ").unwrap();
+            let tail: Vertex = tail_data.parse().unwrap();
+            let edge = Edge::from_str(edge_data);
+            adjacency_list[tail].push(edge);
+        }
+
+        Graph(adjacency_list)
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.0.len() - 1
+    }
+
+    pub fn edges(&self) -> Vec<(Vertex, Vertex, Dist)> {
+        self.0
+            .iter()
+            .enumerate()
+            .skip(1)
+            .flat_map(|(tail, edges)| {
+                edges
+                    .iter()
+                    .map(move |edge| (tail, edge.head, edge.distance))
+            })
+            .collect()
+    }
+
+    // Computes the all pairs shortest paths for the given Graph. Returns Some(PathWeights)
+    // if the graph has no negative cycles. Returns None if there is a negative cycle detected
+    pub fn floyd_warshall(&self) -> PathDistances {
+        let mut current = PathDistances::init(self);
+        for v in 1..=self.len() {
+            let last = current;
+            current = last.update_weights(v)
+        }
+        todo!()
+    }
+}
+
+impl PathDistances {
+    // Given graph, initializes the distances between nodes according to Floyd-Warhsall
+    fn init(graph: &Graph) -> Self {
+        let n = graph.len();
+        let mut weight_data = vec![vec![Distance::Infinite; n + 1]; n + 1];
+        for v in 1..=n {
+            weight_data[v][v] = Distance::Finite(0);
+        }
+
+        for (tail, head, dist) in graph.edges() {
+            weight_data[tail][head] = Distance::Finite(dist);
+        }
+        Self(weight_data)
+    }
+
+    fn new(n: Vertex) -> Self {
+        let mut weight_data = vec![vec![Distance::Infinite; n + 1]; n + 1];
+        PathDistances(weight_data)
+    }
+
+    // Given new_vertex, updates shortest path distances to now include paths with interior node
+    // new_vertex
+    fn update_weights(&self, new_vertex: Vertex) -> PathDistances {
+        todo!()
+    }
+
+    // Computes the distance of the minimum distance path.
+    pub fn min_dist(&self) -> Distance {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::{Distance, Edge, Graph, PathDistances, Vertex};
+
+    impl Edge {
+        fn from(head: Vertex, distance: isize) -> Self {
+            Self { head, distance }
+        }
+    }
+
+    fn graph() -> Graph {
+        Graph(vec![
+            vec![],
+            vec![Edge::from(2, 1)],
+            vec![Edge::from(3, -1)],
+            vec![Edge::from(1, 2)],
+        ])
+    }
+
+    fn negcycle_graph() -> Graph {
+        Graph(vec![
+            vec![],
+            vec![Edge::from(2, 1)],
+            vec![Edge::from(3, -1), Edge::from(4, 0)],
+            vec![Edge::from(1, 2)],
+            vec![Edge::from(2, -1)],
+        ])
+    }
+
+    fn weight_init() -> PathDistances {
+        PathDistances(vec![
+            vec![
+                Distance::Infinite,
+                Distance::Infinite,
+                Distance::Infinite,
+                Distance::Infinite,
+            ],
+            vec![
+                Distance::Infinite,
+                Distance::Finite(0),
+                Distance::Finite(1),
+                Distance::Infinite,
+            ],
+            vec![
+                Distance::Infinite,
+                Distance::Infinite,
+                Distance::Finite(0),
+                Distance::Finite(-1),
+            ],
+            vec![
+                Distance::Infinite,
+                Distance::Finite(2),
+                Distance::Infinite,
+                Distance::Finite(0),
+            ],
+        ])
+    }
+
+    fn weight_init_cycle() -> PathDistances {
+        PathDistances(vec![
+            vec![
+                Distance::Infinite,
+                Distance::Infinite,
+                Distance::Infinite,
+                Distance::Infinite,
+                Distance::Infinite,
+            ],
+            vec![
+                Distance::Infinite,
+                Distance::Finite(0),
+                Distance::Finite(1),
+                Distance::Infinite,
+                Distance::Infinite,
+            ],
+            vec![
+                Distance::Infinite,
+                Distance::Infinite,
+                Distance::Finite(0),
+                Distance::Finite(-1),
+                Distance::Finite(0),
+            ],
+            vec![
+                Distance::Infinite,
+                Distance::Finite(2),
+                Distance::Infinite,
+                Distance::Finite(0),
+                Distance::Infinite,
+            ],
+            vec![
+                Distance::Infinite,
+                Distance::Infinite,
+                Distance::Finite(-1),
+                Distance::Infinite,
+                Distance::Finite(0),
+            ],
+        ])
+    }
+
+    #[test]
+    fn test_parser() {
+        assert_eq!(
+            graph(),
+            Graph::from_str(&fs::read_to_string(&"testgraph.txt").unwrap())
+        )
+    }
+
+    #[test]
+    fn test_parser_cycle() {
+        assert_eq!(
+            negcycle_graph(),
+            Graph::from_str(&fs::read_to_string(&"testgraph_negcycle.txt").unwrap())
+        )
+    }
+
+    #[test]
+    fn test_weight_init() {
+        assert_eq!(PathDistances::init(&graph()), weight_init())
+    }
+
+    #[test]
+    fn test_weight_init_cycle() {
+        assert_eq!(PathDistances::init(&negcycle_graph()), weight_init_cycle())
+    }
+}
--- algorithms/part4/hw1/src/main.rs
+++ algorithms/part4/hw1/src/main.rs
@@ -1,3 +1,20 @@
+mod apsp;
+use apsp::Graph;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let g1 = Graph::from_str(&fs::read_to_string("g1.txt").unwrap());
+    let g2 = Graph::from_str(&fs::read_to_string("g2.txt").unwrap());
+    let g3 = Graph::from_str(&fs::read_to_string("g3.txt").unwrap());
+
+    let g1_dist = g1.floyd_warshall();
+    let g2_dist = g2.floyd_warshall();
+    let g3_dist = g3.floyd_warshall();
+
+    let min_dist = g1_dist
+        .min_dist()
+        .min(&g2_dist.min_dist())
+        .min(&g3_dist.min_dist());
+
+    println!("Minimum distance path has length: {:?}", min_dist)
 }
