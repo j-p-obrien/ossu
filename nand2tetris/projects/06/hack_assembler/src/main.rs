b64e21edb0f35a1f984075bf2ac4ffc5913e6a6a
JP O'Brien <jpobrien925@gmail.com>
2025-01-08 17:42:52 -0800

    Working on Parser.

--- nand2tetris/projects/06/hack_assembler/src/main.rs
+++ nand2tetris/projects/06/hack_assembler/src/main.rs
@@ -1,29 +1,15 @@
 use std::{
+    cell::Cell,
+    collections::HashMap,
     iter::{Enumerate, Peekable},
+    rc::Rc,
     str::Bytes,
 };
 
-struct AddressInstruction {
-    address: u16,
-}
+type Address = u16;
 
-enum Mode {
-    A,
-    M,
-}
-
-struct Destination(u8);
-
-struct Computation(u8);
-
-struct Comparison(u8);
-
-struct ComputationInstruction {
-    mode: Mode,
-    destination: Destination,
-    computation: Computation,
-    comparison: Comparison,
-}
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
+struct Identifier<'a>(&'a str);
 
 #[derive(Debug)]
 enum Token<'a> {
@@ -50,7 +36,7 @@ enum Token<'a> {
     KBD,
     // Other stuff
     Number(u16),
-    Identifier(&'a str),
+    Identifier(Identifier<'a>),
 }
 
 impl<'a> From<&'a [u8]> for Token<'a> {
@@ -68,14 +54,15 @@ impl<'a> From<&'a [u8]> for Token<'a> {
             _ => {
                 // SAFETY: We converted this from &str earlier.
                 let name = unsafe { std::str::from_utf8_unchecked(value) };
-                Token::Identifier(name)
+                Token::Identifier(Identifier(name))
             }
         }
     }
 }
 
+#[derive(Debug, Clone)]
 struct Lexer<'a> {
-    input: &'a [u8],
+    file_contents: &'a [u8],
     file_iter: Peekable<Enumerate<Bytes<'a>>>,
 }
 
@@ -110,13 +97,13 @@ impl<'a> Iterator for Lexer<'a> {
 impl<'a> Lexer<'a> {
     fn new(file_contents: &'a str) -> Self {
         Self {
-            input: file_contents.as_bytes(),
+            file_contents: file_contents.as_bytes(),
             file_iter: file_contents.bytes().enumerate().peekable(),
         }
     }
 
-    // Returns Some(first byte) that isn't part of a comment or whitespace, if there is one.
-    // Otherwise, returns None.
+    // Returns Some(byte) where byte is the first byte that isn't part of a comment or whitespace,
+    // if there is one. Otherwise, returns None.
     fn skip_comments_and_whitespace(&mut self) -> Option<(usize, u8)> {
         loop {
             let (pos, byte) = self.file_iter.next()?;
@@ -143,7 +130,7 @@ impl<'a> Lexer<'a> {
         {
             end = pos
         }
-        Token::from(&self.input[start..=end])
+        Token::from(&self.file_contents[start..=end])
     }
 
     fn get_number(&mut self, start: usize) -> Token<'a> {
@@ -154,11 +141,149 @@ impl<'a> Lexer<'a> {
         // SAFETY: We converted this &[u8] from a str in the beginning AND we know that all the
         // values are digits.
         let number = unsafe {
-            std::str::from_utf8_unchecked(&self.input[start..=end])
+            std::str::from_utf8_unchecked(&self.file_contents[start..=end])
                 .parse()
                 .unwrap_unchecked()
         };
         Token::Number(number)
+    }
+}
+
+struct LabelTable<'a>(HashMap<Identifier<'a>, AddressInstruction>);
+
+impl<'a> LabelTable<'a> {
+    fn new() -> Self {
+        Self(HashMap::new())
+    }
+}
+
+struct SymbolTable<'a> {
+    table: HashMap<Identifier<'a>, Address>,
+    next_address: Address,
+}
+
+impl<'a> SymbolTable<'a> {
+    fn new() -> Self {
+        let table = HashMap::new();
+        Self {
+            table,
+            next_address: 16,
+        }
+    }
+}
+
+// !!! Symbols are lower priority than labels.
+struct Parser<'a> {
+    lexer: Lexer<'a>,
+    instructions_parsed: Address,
+    labels: LabelTable<'a>,
+    symbols: SymbolTable<'a>,
+}
+
+#[derive(Debug)]
+enum AddressInstruction {
+    Definite(Address),
+    // Sometimes we come across a label in an '@' command before the label itself is defined.
+    // Thus, we use a shared reference so we can update the value without a second pass over
+    // the file.
+    Indefinite(Rc<Cell<Option<Address>>>),
+}
+
+#[derive(Debug)]
+enum Mode {
+    A,
+    M,
+}
+
+#[derive(Debug)]
+struct Destination(Address);
+
+#[derive(Debug)]
+struct Computation(Address);
+
+#[derive(Debug)]
+struct Jump(Address);
+
+#[derive(Debug)]
+struct ComputationInstruction {
+    mode: Mode,
+    destination: Destination,
+    computation: Computation,
+    comparison: Jump,
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Computation(ComputationInstruction),
+    Address(AddressInstruction),
+}
+
+impl<'a> Parser<'a> {
+    fn new(lexer: Lexer<'a>) -> Self {
+        Self {
+            lexer,
+            instructions_parsed: 0,
+            labels: LabelTable::new(),
+            symbols: SymbolTable::new(),
+        }
+    }
+
+    fn add_label(&mut self, label: Identifier<'a>) {
+        todo!()
+    }
+
+    fn label_instruction(&mut self) {
+        let label_token = self.lexer.next().expect("Didn't expect EOF.");
+        if let Token::Identifier(label) = label_token {
+            self.add_label(label);
+        } else {
+            panic!(
+                "Expected a label at instruction: {}.",
+                self.instructions_parsed
+            )
+        }
+    }
+
+    fn address_instruction(&mut self) -> AddressInstruction {
+        let address = self.lexer.next().expect("Didn't expect EOF.");
+        match address {
+            Token::R(num) => todo!(),
+            Token::SP => todo!(),
+            Token::LCL => todo!(),
+            Token::ARG => todo!(),
+            Token::THIS => todo!(),
+            Token::THAT => todo!(),
+            Token::SCREEN => todo!(),
+            Token::KBD => todo!(),
+            Token::Number(_) => todo!(),
+            Token::Identifier(identifier) => todo!(),
+            _ => panic!(),
+        }
+        todo!()
+    }
+
+    fn computation_instruction(&mut self) -> ComputationInstruction {
+        todo!()
+    }
+}
+
+impl<'a> Iterator for Parser<'a> {
+    type Item = Instruction;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut token = self.lexer.next()?;
+        // This allows multiple label instructions in a row, which I think is technically allowed?
+        // I don't see anything in the spec forbidding it, so I suppose we will allow it.
+        while let Token::LeftParenthesis = token {
+            self.label_instruction();
+            token = self.lexer.next()?;
+        }
+        Some(match token {
+            Token::AtSymbol => Instruction::Address(self.address_instruction()),
+            Token::Number(_) => Instruction::Computation(self.computation_instruction()),
+            Token::Identifier(_) => Instruction::Computation(self.computation_instruction()),
+            _ => todo!(),
+        })
     }
 }
 
@@ -174,6 +299,9 @@ fn atoi(byte: u8) -> u8 {
 fn main() {
     let file_contents = std::fs::read_to_string("../max/Max.asm").expect("Path not found.");
     let lexer = Lexer::new(&file_contents);
+    let parser = Parser::new(lexer.clone());
     let lexed_file: Vec<Token<'_>> = lexer.collect();
-    print!("{:#?}", lexed_file)
+    let parsed_file: Vec<Instruction> = parser.collect();
+    print!("{:#?}", lexed_file);
+    print!("{:#?}", parsed_file)
 }
